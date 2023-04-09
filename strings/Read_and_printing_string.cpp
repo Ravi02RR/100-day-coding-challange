859f1a9ed055e729cdbb81faa5ae770cd4cbe9d0
Ravi Ranjan <ravi848101mnb@gmail.com>
2023-04-09 20:19:31 +0530

    first commit

---
+++ strings/Read_and_printing_string.cpp
@@ -0,0 +1,34 @@
+// Here is an example code block that reads a string from standard input and prints it to the standard output in C++:
+
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// int main() {
+//     string s;
+//     cout << \"Enter a string: ";
+//     getline(cin, s);
+//     cout << \"You entered: " << s << endl;
+//     return 0;
+// }
+
+// This code declares a string s and prompts the user to enter a string using the cout object and the getline function. The input string is then printed to the standard output using the cout object again. The getline function reads the entire line of input, including any whitespace characters, and stores it in the string s. The using namespace std; statement is used to signify that we are using the standard library in our program.
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     char s[100];
+//     char s2[100];
+//     cout << "Enter a string: ";
+//     cin.getline(s, 100);
+
+//     cout << "Enter a string: ";
+//     cin.getline(s2, 100);
+//     // cin.getline(s, 100);
+//     cout << "You entered: " << s2 << endl;
+//     return 0;
+// }
---
+++ strings/string.cpp
@@ -0,0 +1,80 @@
+// In C++, a string is a sequence of characters. It is represented by the std::string class, which is part of the standard library. A string object can be constructed using a string literal or by copying another string object. It is mutable, meaning that you can modify the individual characters in a string. The string class also provides a wide range of member functions to manipulate strings such as concatenation, substring, size, find, replace, and much more. Here is an example:
+
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// int main() {
+//     string s1 = "Hello";
+//     string s2 = " World";
+//     string s3 = s1 + s2;
+//     cout << s3 << endl;
+//     return 0;
+// }
+
+// Output: Hello World
+
+// we can use it in two ways
+
+// 1.using char array
+// 2.using string calss
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     char S[10] = "Hello";
+//     cout << S << endl;
+
+// we can use empty string too.....
+
+//     char S[] = "Hello";
+//     cout << S << endl;
+//     return 0;
+// }
+
+// using alphabatical order
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     char S[] = {'h', 'e', 'l', 'l', 'o', '\0', 'p', 'p', 'p'};
+//     cout << S;
+//     return 0;
+// }
+
+//'\0' stops at null char
+
+// string printing by ASCII code
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     char S[] = {65, 66, 67, 68, '\0'};
+//     cout << S << endl;
+//     return 0;
+// }
+
+// output:ABCD
+
+// using pointers
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     char *S = "hello";
+//     string s = "hello";
+//     cout << S << endl;
+//     return 0;
+// }
