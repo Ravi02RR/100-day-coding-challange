e1a3586705302d5ac95bbaead989028bff24451a
Ravi Ranjan <ravi848101mnb@gmail.com>
2023-05-18 22:55:13 +0530

    array

---
+++ Day 10/Array Rev/implementation.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+void PrintArray()
+{
+}
+
+int main()
+{
+    // declare
+    int number[15];
+
+    cout << "value at zero index  " << number[0] << endl;
+
+    cout << "array fromed" << endl;
+
+    // array
+    int second[3] = {5, 7, 11};
+
+    return 0;
+}
