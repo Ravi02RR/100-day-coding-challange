db5ae451ad726f052a48f088ab4c91928db22e41
Ravi Ranjan <ravi848101mnb@gmail.com>
2023-05-26 11:41:27 +0530

    --

--- Day 14/frequency.cpp
+++ Day 14/frequency.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 using namespace std;
 
 int main()
@@ -6,14 +7,14 @@ int main()
     int a[] = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
     int size = sizeof(a) / sizeof(a[0]);
 
-    int frequency[INT16_MAX] = {0};
+    int frequency [size] = {0};
 
     for (int i = 0; i < size; i++)
     {
         frequency[a[i]]++;
     }
 
-    for (int i = 0; i <= size; i++)
+    for (int i = 0; i <size; i++)
     {
         if (frequency[i] != 0)
         {
