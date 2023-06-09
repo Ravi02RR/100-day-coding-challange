9f940fda58e12759137899f90edcd9c131b9767c
Ravi Ranjan <ravi848101mnb@gmail.com>
2023-06-10 01:16:40 +0530

    binary_search_done

--- Day 17/optimizeBS.cpp
+++ Day 17/optimizeBS.cpp
@@ -6,7 +6,7 @@ int binarSearch(int arr[], int size, int key)
     int start = 0;
     int end = size - 1;
 
-    int mid = (start + end) / 2;
+    int mid = start +((end-start)/2);
 
     while (start < end)
     {
@@ -25,7 +25,7 @@ int binarSearch(int arr[], int size, int key)
         {
             end = mid - 1;
         }
-        mid = (start + end) / 2;
+        mid = start +((end-start)/2);
     }
     return -1;
 }
@@ -40,5 +40,17 @@ int main()
     cout << "the index of even is " << evenindex << endl;
     cout << "the index of odd is " << oddindex << endl;
 
+
+    int arry[]={34,21,335,46,57,6474};
+   
+
     return 0;
 }
+
+
+
+//to make it error free
+//start + end) / 2
+
+//👆  
+//start +((end-start)/2)
