d28d7396778080054a8378427843d286a95c3d96
Ravi Ranjan <ravi848101mnb@gmail.com>
2023-07-01 23:16:08 +0530

    bs

--- Day 24/BUBBLESORT.cpp
+++ Day 24/BUBBLESORT.cpp
@@ -2,19 +2,20 @@
 // // a>b then swap(a,b)....continuee..
 // until no more swaps are needed.
 
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+void bubbleSort(vector<int> &arr, int n)
+{
+    for (int i = 1; i < n; i++)
+    {
 
-#include <bits/stdc++.h> 
-void bubbleSort(vector<int>& arr, int n)
-{   
-    for(int i=1;i<n;i++){
-
-        for(int j=0;j<n-i;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
+        for (int j = 0; j < n - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
             }
-
         }
     }
 }
-
-
